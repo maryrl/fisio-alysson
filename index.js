64419d0bb6d2438e8e62d647c8d581381791b66d
M4ryrl <81182341+maryrl@users.noreply.github.com>
2024-12-17 09:30:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ document.getElementById('form-consulta').addEventListener('submit', function (e)
     // Redirecionar para o WhatsApp
     window.open(urlWhatsApp, '_blank');
   });
+
+
+     let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let slides = document.getElementsByClassName("imagem-modalidade");
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+            slides[slideIndex - 1].style.display = "block";  
+            setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+        }
+
+        function moveSlide(n) {
+            slideIndex += n;
+            let slides = document.getElementsByClassName("imagem-modalidade");
+            if (slideIndex > slides.length) {slideIndex = 1}
+            if (slideIndex < 1) {slideIndex = slides.length}
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            slides[slideIndex - 1].style.display = "block";  
+        }
